74e2713b770e09b029df9a651341ce740df3d72d
Maryam Rizwan <165351146+MaryamRizwan7@users.noreply.github.com>
2026-06-24 17:09:42 +0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#9B0020",
+        secondary: "#f5f5f5",
+        light: "#ffffff",
+        accent: "#9B0020",
+      },
+    },
+  },
+  plugins: [],
+};
